910b71acac12e9ea92abd8be336cde76173f97b2
Sakhawath Hossain <72663172+sakhawath162@users.noreply.github.com>
2020-10-28 16:24:58 +0600

    Templates

---
+++ template/footer.php
@@ -0,0 +1,13 @@
+      	<hr>
+
+      	<footer>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Admin Login</a> 2019
+        	</div>
+      	</footer>
+    </div>
+
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,68 @@
+<?php
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Bookstore</a>
+        </div>
+
+
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Publisher</a></li>
+
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp;Latest Books</a></li>
+
+              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+
+              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>
+              <?php if( ! isset( $_SESSION["user_id"]) ) { ?>
+              <li><a href="signup.php"><span class="glyphicon glyphicon-log-in"></span>&nbsp; Sign in</a></li>
+              <?php } ?>
+              <?php if( isset( $_SESSION["user_id"]) ) { ?>
+              <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp; logout</a></li>
+              <?php } ?>
+              <li><a href="map.php"><span class="glyphicon glyphicon-map"></span>&nbsp; Locate Store</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <?php
+      if(isset($title) && $title == "Index") {
+    ?>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Welcome to online bookstore</h1>
+      </div>
+    </div>
+    <?php } ?>
+
+    <div class="container" id="main">
